6a92e97af418d064b1169bae249b825cc3660023
Abdul Rehman Khalil <abdulrehmankhalil@abduls-air.home>
2021-06-19 12:14:58 +0100

    potions working

---
+++ __test__/potion.test.js
@@ -0,0 +1,17 @@
+
+const Potion = require('../lib/Potion.js')
+
+test('create a health potion object', () => {
+  const potion = new Potion('Health');
+
+  expect(potion.name).toBe('Health');
+  expect(potion.value).toEqual(expect.any(Number));
+})
+
+test('create a health potion object', () => {
+  const potion = new Potion('');
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
